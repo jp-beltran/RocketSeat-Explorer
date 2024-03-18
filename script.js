d4132c53bd9f0e89c1bdda421c6acec6804e03bd
Joao Pedro <jpbcs2005@gmail.com>
2024-03-18 17:05:31 -0300

    change background based on media queries

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function toggleMode(){
 
     const img = document.querySelector("#profile img")
 
-    if(html.classList.contains("light")) {
+    /*if(html.classList.contains("light")) {
 
         img.setAttribute("src", "./assets/images/avatar-light.png")
 
     } else {
         img.setAttribute("src", "./assets/images/avatar-light.png")
     }
-
+*/
 }
